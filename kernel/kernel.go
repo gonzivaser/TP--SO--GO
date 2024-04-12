14d7470188e664d1415352146946ef730659616e
gadstamati <gadstam71@gmail.com>
2024-04-12 18:47:31 -0300

    Refactor kernel main function to include HTTP handlers for PUT /process and POST /helloworld endpoints

--- kernel/kernel.go
+++ kernel/kernel.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/sisoputnfrba/tp-golang/kernel/utils"
+)
+
+func main() {
+	http.HandleFunc("PUT /process", utils.IniciarProceso)
+	http.HandleFunc("POST /helloworld", utils.HelloWorld)
+	//http.HandleFunc("DELETE /process/{pid}", utils.FinalizarProceso)
+	//http.HandleFunc("GET /process/{pid}", utils.EstadoProceso)
+	//http.HandleFunc("PUT /plani", utils.IniciarPlanificacion)
+	//http.HandleFunc("DELETE /plani", utils.DetenerPlanificacion)
+	//http.HandleFunc("GET /process", utils.ListarProcesos)
+	http.ListenAndServe(":8080", nil)
 }
