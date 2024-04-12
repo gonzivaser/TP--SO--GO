389fd5f49d3fba70dafb0b9b410f6aa27287905a
gadstamati <gadstam71@gmail.com>
2024-04-12 17:49:16 -0300

    error  building

--- kernel/server/main.go
+++ kernel/server/main.go
@@ -6,6 +6,12 @@ import (
 )
 
 func main() {
+	http.HandleFunc("PUT /process", utils.IniciarProceso)
 	http.HandleFunc("POST /helloworld", utils.HelloWorld)
+	//http.HandleFunc("DELETE /process/{pid}", utils.FinalizarProceso)
+	//http.HandleFunc("GET /process/{pid}", utils.EstadoProceso)
+	//http.HandleFunc("PUT /plani", utils.IniciarPlanificacion)
+	//http.HandleFunc("DELETE /plani", utils.DetenerPlanificacion)
+	//http.HandleFunc("GET /process", utils.ListarProcesos)
 	http.ListenAndServe(":8080", nil)
 }
--- kernel/server/utils/utils.go
+++ kernel/server/utils/utils.go
@@ -8,7 +8,10 @@ import (
 
 type BodyRequest struct {
 	Name string `json:"name"`
+	Path string `json:"path"`
 }
+
+var pid int = 0
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
 	var request BodyRequest
@@ -26,4 +29,29 @@ func HelloWorld(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	w.Write(respuesta)
+}
+
+//Listar Procesos: Se encargará de mostrar por consola y retornar por la api el listado de procesos que se encuentran en el sistema con su respectivo
+//estado dentro de cada uno de ellos.
+
+func IniciarProceso(w http.ResponseWriter, r *http.Request) {
+	var request BodyRequest
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	respuesta, err := json.Marshal(fmt.Sprintf("pid: %d", pid))
+	if err != nil {
+		http.Error(w, "Error al codificar los datos como JSON", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(respuesta)
+}
+
+func ListarProcesos(w http.ResponseWriter, r *http.Request) {
+
 }
