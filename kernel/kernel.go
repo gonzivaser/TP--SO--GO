2f52ca5fc768efc673c3c8e6997505aa9780fcc2
Alan Garber <alangarber1@gmail.com>
2024-04-14 11:09:38 -0300

    added API endpoints for kernel and obligatory logs

--- kernel/kernel.go
+++ kernel/kernel.go
@@ -7,12 +7,12 @@ import (
 )
 
 func main() {
+	utils.ConfigurarLogger()
 	http.HandleFunc("PUT /process", utils.IniciarProceso)
-	http.HandleFunc("POST /helloworld", utils.HelloWorld)
-	//http.HandleFunc("DELETE /process/{pid}", utils.FinalizarProceso)
-	//http.HandleFunc("GET /process/{pid}", utils.EstadoProceso)
-	//http.HandleFunc("PUT /plani", utils.IniciarPlanificacion)
-	//http.HandleFunc("DELETE /plani", utils.DetenerPlanificacion)
-	//http.HandleFunc("GET /process", utils.ListarProcesos)
+	http.HandleFunc("DELETE /process/{pid}", utils.FinalizarProceso)
+	http.HandleFunc("GET /process/{pid}", utils.EstadoProceso)
+	http.HandleFunc("PUT /plani", utils.IniciarPlanificacion)
+	http.HandleFunc("DELETE /plani", utils.DetenerPlanificacion)
+	http.HandleFunc("GET /process", utils.ListarProcesos)
 	http.ListenAndServe(":8080", nil)
 }
--- kernel/utils/utils.go
+++ kernel/utils/utils.go
@@ -3,49 +3,118 @@ package utils
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"os"
 )
 
+type BodyResponseListProcess struct {
+	Pid   int    `json:"pid"`
+	State string `json:"state"`
+}
+
+type BodyResponsePid struct {
+	Pid int `json:"pid"`
+}
+
+type BodyResponseState struct {
+	State string `json:"state"`
+}
+
 type BodyRequest struct {
-	Name string `json:"name"`
 	Path string `json:"path"`
 }
 
-var pid int = 0
-
-func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	var request BodyRequest
-	// MANDO LA REQUEST Y DECODIFICO
-	err := json.NewDecoder(r.Body).Decode(&request)
-	// ESTE IF ESTA POR SI NO PUDO PEGAR A LA API
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Rompi todo %s", err.Error()), http.StatusBadRequest)
-		return
-	}
-	// EN CASO DE QUE PASA ESE IF, IMPRIMO HOLA (NOMBRE) COMO ANDAS
-	respuesta, err := json.Marshal(fmt.Sprintf("Hola %s! Como andas?", request.Name))
-
-	// ESTE IF ESTA EN CASO DE QUE POR EJEMPLO EL NOMBRE ESTE MAL Y NO PUEDA DECODIFICARLO
-	if err != nil {
-		http.Error(w, "Error al codificar los datos como JSON", http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	w.Write(respuesta)
-}
-
-//Listar Procesos: Se encargará de mostrar por consola y retornar por la api el listado de procesos que se encuentran en el sistema con su respectivo
-//estado dentro de cada uno de ellos.
-
 func IniciarProceso(w http.ResponseWriter, r *http.Request) {
 
-	respuesta, err := json.Marshal(fmt.Sprintf("pid: %d", pid))
+	var request BodyRequest
+	err := json.NewDecoder(r.Body).Decode(&request)
 	if err != nil {
 		http.Error(w, "Error al codificar los datos como JSON", http.StatusInternalServerError)
 		return
 	}
 
+	BodyResponse := BodyResponsePid{
+		Pid: 0,
+	}
+
+	pidResponse, _ := json.Marshal(BodyResponse)
+
+	log.Printf("Se crea el proceso %d en NEW", BodyResponse.Pid)
+
 	w.WriteHeader(http.StatusOK)
-	w.Write(respuesta)
+	w.Write(pidResponse)
+}
+
+func FinalizarProceso(w http.ResponseWriter, r *http.Request) {
+
+	pid := r.PathValue("pid")
+
+	log.Printf("Finaliza el proceso %s - Motivo: <SUCCESS / INVALID_RESOURCE / INVALID_WRITE>", pid)
+
+	respuestaOK := fmt.Sprintf("Proceso finalizado:%s", pid)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(respuestaOK))
+}
+
+func EstadoProceso(w http.ResponseWriter, r *http.Request) {
+	pid := r.PathValue("pid")
+
+	BodyResponse := BodyResponseState{
+		State: "EXIT",
+	}
+
+	stateResponse, _ := json.Marshal(BodyResponse)
+
+	log.Printf("PID: %s - Estado Anterior: <ESTADO_ANTERIOR> - Estado Actual: <ESTADO_ACTUAL>", pid) // A checkear
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(stateResponse)
+}
+
+func IniciarPlanificacion(w http.ResponseWriter, r *http.Request) {
+	log.Printf("PID: <PID> - Bloqueado por: <INTERFAZ / NOMBRE_RECURSO>") //ESTO NO VA ACA
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Planificación iniciada"))
+}
+
+func DetenerPlanificacion(w http.ResponseWriter, r *http.Request) {
+	log.Printf("PID: <PID> - Desalojado por fin de Quantum") //ESTO NO VA ACA
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Planificación detenida"))
+}
+
+func ListarProcesos(w http.ResponseWriter, r *http.Request) {
+	BodyResponse := []BodyResponseListProcess{
+		{0, "EXEC"},
+		{1, "READY"},
+		{2, "BLOCK"},
+		{3, "FIN"},
+	}
+
+	arrayProcesos, err := json.Marshal(BodyResponse)
+	if err != nil {
+		http.Error(w, "Error al codificar los datos como JSON", http.StatusInternalServerError)
+		return
+	}
+
+	for i := 0; i < len(BodyResponse); i++ {
+		fmt.Print(BodyResponse[i], "\n")
+	}
+
+	log.Print("Cola Ready <COLA>: [<LISTA DE PIDS>]") //ESTO NO VA ACA
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(arrayProcesos)
+}
+
+func ConfigurarLogger() {
+	logFile, err := os.OpenFile("tp0.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	if err != nil {
+		panic(err)
+	}
+	mw := io.MultiWriter(os.Stdout, logFile)
+	log.SetOutput(mw)
 }
