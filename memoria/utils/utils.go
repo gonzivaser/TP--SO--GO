a80d6951704d1add47d2b5ad0c1d16ad08b6df17
gadstamati <gadstam71@gmail.com>
2024-04-29 16:49:32 -0300

    file readed and printed

--- memoria/utils/utils.go
+++ memoria/utils/utils.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -42,7 +43,7 @@ func ConfigurarLogger() {
 }
 
 func ProcessSavedPathFromKernel(w http.ResponseWriter, r *http.Request) {
-
+	//globals.ClientConfig = IniciarConfiguracion("config.json")
 	log.Printf("Recibiendo solicitud de path desde el kernel")
 	var savedPath BodyRequest
 	err := json.NewDecoder(r.Body).Decode(&savedPath)
@@ -51,12 +52,26 @@ func ProcessSavedPathFromKernel(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	log.Printf("Path recibido desde el kernel: %s", savedPath.Path)
+
+	file, err := os.Open(savedPath.Path)
+	check(err)
+
+	fi, err := file.Stat()
+	check(err)
+
+	sliceBytes := make([]byte, fi.Size())      //Esta línea crea un slice de bytes
+	numBytesRead, err := file.Read(sliceBytes) //es el número de bytes leídos
+	check(err)
+	fmt.Printf("%d bytes: %s\n", numBytesRead, string(sliceBytes[:numBytesRead])) //Esta línea imprime el número de bytes leídos (numBytesRead) y el contenido del slice de bytes sliceBytes. string(sliceBytes[:numBytesRead]) convierte el slice de bytes en una cadena y solo imprime los primeros numBytesRead bytes leídos, ya que el slice puede tener una capacidad mayor que la cantidad real de bytes leídos.
+
+	file.Close()
+
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(savedPath.Path))
 }
 
-/*func check(e error) {
+func check(e error) {
 	if e != nil {
 		panic(e)
 	}
-}*/
+}
