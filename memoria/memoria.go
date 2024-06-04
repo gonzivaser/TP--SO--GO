4863863a1f8009060d3a5d97eedbbb92064366a3
Alan Garber <alangarber1@gmail.com>
2024-06-04 15:46:49 -0300

    added http routes

--- memoria/memoria.go
+++ memoria/memoria.go
@@ -20,7 +20,12 @@ func main() {
 	puerto := globals.ClientConfig.Puerto
 
 	http.HandleFunc("POST /setInstructionFromFileToMap", utils.SetInstructionsFromFileToMap)
-	http.HandleFunc("GET /getInstructionFromPid", utils.GetInstruction)
+	http.HandleFunc("GET  /getInstructionFromPid", utils.GetInstruction)
+	http.HandleFunc("POST /createProcess", utils.CreateProcessHandler)
+	http.HandleFunc("POST /terminateProcess", utils.TerminateProcessHandler)
+	http.HandleFunc("POST /resizeProcess", utils.ResizeProcessHandler)
+	http.HandleFunc("POST /readMemory", utils.ReadMemoryHandler)
+	http.HandleFunc("POST /writeMemory", utils.WriteMemoryHandler)
 
 	http.ListenAndServe(":"+strconv.Itoa(puerto), nil)
 }
