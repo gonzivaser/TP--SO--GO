509b8d18c6539915c2bb3ef83db1aec2449deaee
AlanGarber <alangarber1@gmail.com>
2024-07-26 02:58:49 +0000

    final refacto crear_proceso

--- ejecutablesSH/crear_proceso.sh
+++ ejecutablesSH/crear_proceso.sh
@@ -1,36 +1,24 @@
 #!/bin/bash
 
-# Verificar si se ha definido la variable KERNEL_PORT
-if [ -z "$KERNEL_PORT" ]; then
-    echo "No se ha definido la variable KERNEL_PORT"
-    echo "Usando puerto por defecto 8080"
-    KERNEL_PORT=8080
-fi
-
-# Verificar si se ha definido la variable KERNEL_HOST
-if [ -z "$KERNEL_HOST" ]; then
-    echo "No se ha definido la variable KERNEL_HOST"
-    echo "Usando HOST por defecto localhost"
-    KERNEL_HOST=localhost
-fi
-
-KERNEL_URL="http://$KERNEL_HOST:$KERNEL_PORT/process"
-
 # Verificar si se pasaron los argumentos necesarios
 if [ "$#" -ne 2 ]; then
     echo "Uso: $0 <PID> <PATH>"
     exit 1
 fi
 
+# Asignar los argumentos a variables
 PID="$1"
-PATH="$2"
+FILE_PATH="$2"
+
+# URL del servidor
+URL="http://192.168.0.91:8080/process"
 
 # Cuerpo JSON
 BODY="{\"pid\": $PID, \"path\": \"$FILE_PATH\"}"
 
-echo "URL: $KERNEL_URL"
+# Imprimir la URL y el cuerpo JSON para depuración
+echo "URL: $URL"
 echo "Cuerpo JSON: $BODY"
 
 # Realizar la petición PUT con curl
-curl -X PUT "${KERNEL_URL}" -H "Content-Type: application/json" -d "${BODY}"
-
+curl -X PUT "$URL" -H "Content-Type: application/json" -d "$BODY"
