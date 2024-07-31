a41bf8af053478940a47ee7aebed831ea730e860
Fausto Oliva <fausto.calavera@gmail.com>
2024-07-31 16:28:28 -0300

    CPU & MEMORY

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -12,18 +12,17 @@ import (
 
 func main() {
 	utils.ConfigurarLogger()
-	globals.ClientConfig = utils.IniciarConfiguracion(os.Args[1])
+	globals.ClientConfig = utils.InitializeConfiguracion(os.Args[1])
 
 	if globals.ClientConfig == nil {
 		log.Fatalf("No se pudo cargar la configuración")
 	}
 	puerto := globals.ClientConfig.Puerto
 
-	http.HandleFunc("/receivePCB", utils.ReceivePCB)
-	http.HandleFunc("POST /receiveDataFromMemory", utils.RecieveMOV_IN)
-	http.HandleFunc("/interrupt", utils.Checkinterrupts)
-	http.HandleFunc("/translate", utils.TranslateHandler)
-	http.HandleFunc("/recievePageTam", utils.ReceiveTamPage)
+	http.HandleFunc("/receivePCB", utils.ReceiveContextFromKernel)
+	http.HandleFunc("POST /receiveDataFromMemory", utils.RecieveMOV_INFromMemory)
+	http.HandleFunc("/interrupt", utils.CheckinterruptsFromKernel)
+	http.HandleFunc("/recievePageTam", utils.ReceiveTamPageFromMemory)
 	http.HandleFunc("POST /recieveFrame", utils.RecieveFramefromMemory)
 	http.ListenAndServe(":"+strconv.Itoa(puerto), nil)
 }
